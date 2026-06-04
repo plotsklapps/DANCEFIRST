3a599ef7179309c384a6de7c4744d7207632ca5c
plotsklapps <plotsklapps@gmail.com>
2026-06-04 18:06:05 +0200

    - LoadingScreen now robust.

--- lib/screens/loadingscreen/loading_screen.dart
+++ lib/screens/loadingscreen/loading_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:dancefirst/screens/homescreen/home_screen.dart';
 import 'package:dancefirst/screens/loadingscreen/login_screen.dart';
 import 'package:dancefirst/screens/loadingscreen/verification_screen.dart';
@@ -7,100 +5,79 @@ import 'package:dancefirst/services/firestore_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-// LoadingScreen is the brain for appstart and auth-flow.class
-class LoadingScreen extends StatefulWidget {
+class LoadingScreen extends StatelessWidget {
   const LoadingScreen({super.key});
 
-  @override
-  State<LoadingScreen> createState() {
-    return _LoadingScreenState();
-  }
-}
+  Future<Widget> _determineNextScreen(User? user) async {
+    await Future<void>.delayed(const Duration(seconds: 2));
 
-class _LoadingScreenState extends State<LoadingScreen> {
-  late StreamSubscription<User?> _authSubscription;
-
-  @override
-  void initState() {
-    super.initState();
-    // Listen to auth changes to reinitialize.
-    _authSubscription = FirebaseAuth.instance.authStateChanges().listen((_) {
-      unawaited(_initializeApp());
-    });
-    // Trigger initialization immediately.
-    unawaited(_initializeApp());
-  }
-
-  @override
-  void dispose() {
-    unawaited(_authSubscription.cancel());
-    super.dispose();
-  }
-
-  Future<void> _initializeApp() async {
-    // Delay 2 sec and fetch state.
-    final List<dynamic> results = await Future.wait(<Future<dynamic>>[
-      Future<void>.delayed(const Duration(seconds: 2)),
-      _fetchAppState(),
-    ]);
-
-    final AppState state = results[1] as AppState;
-
-    if (!mounted) return;
-
-    // Navigate based on fetched state.
-    await Navigator.of(context).pushReplacement(
-      MaterialPageRoute<void>(
-        builder: (_) {
-          return state.nextScreen;
-        },
-      ),
-    );
-  }
-
-  Future<AppState> _fetchAppState() async {
-    final FirebaseAuth auth = FirebaseAuth.instance;
-    final User? user = auth.currentUser ?? await auth.authStateChanges().first;
-
-    // User not logged in.
+    // User is not logged in.
     if (user == null) {
-      return AppState(const LoginScreen());
+      return const LoginScreen();
     }
 
-    // Fetch latest status.
     await user.reload();
 
-    // User not verified.
+    // User is not verified.
     if (!user.emailVerified) {
-      return AppState(VerificationScreen(user: user));
+      return VerificationScreen(user: user);
     }
 
-    // Fetch user role (admin/client).
+    // Fetch userRole (admin/client).
     final String role = await FirestoreService().getUserRole();
-
-    // Navigate to HomeScreen.
-    return AppState(HomeScreen(role: role));
+    return HomeScreen(role: role);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset('assets/dfLogoBlack.png', width: 200),
-            const SizedBox(height: 32),
-            const SizedBox(width: 200, child: LinearProgressIndicator()),
-          ],
-        ),
-      ),
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (BuildContext context, AsyncSnapshot<User?> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Scaffold(
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Image.asset('assets/dfLogoBlack.png', width: 200),
+                  const SizedBox(height: 32),
+                  const SizedBox(
+                    width: 200,
+                    child: LinearProgressIndicator(),
+                  ),
+                ],
+              ),
+            ),
+          );
+        }
+
+        final User? user = snapshot.data;
+
+        return FutureBuilder<Widget>(
+          future: _determineNextScreen(user),
+          builder:
+              (BuildContext context, AsyncSnapshot<Widget> screenSnapshot) {
+                if (!screenSnapshot.hasData) {
+                  return Scaffold(
+                    body: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Image.asset('assets/dfLogoBlack.png', width: 200),
+                          const SizedBox(height: 32),
+                          const SizedBox(
+                            width: 200,
+                            child: LinearProgressIndicator(),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                }
+                return screenSnapshot.data!;
+              },
+        );
+      },
     );
   }
-}
-
-class AppState {
-  AppState(this.nextScreen);
-
-  final Widget nextScreen;
 }
--- lib/screens/loadingscreen/login_screen.dart
+++ lib/screens/loadingscreen/login_screen.dart
@@ -43,116 +43,119 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       body: Center(
         child: ScrollService(
-          child: Card(
-            clipBehavior: Clip.hardEdge,
-            child: Column(
-              children: <Widget>[
-                // HEADER.
-                Container(
-                  height: 140,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: theme.colorScheme.primary,
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 64),
-                    child: Image.asset(
-                      'assets/dfLogoWhite.png',
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 32),
+            child: Card(
+              clipBehavior: Clip.hardEdge,
+              child: Column(
+                children: <Widget>[
+                  // HEADER.
+                  Container(
+                    height: 140,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: theme.colorScheme.primary,
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 64),
+                      child: Image.asset(
+                        'assets/dfLogoWhite.png',
+                      ),
                     ),
                   ),
-                ),
-                // FORM.
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(32, 24, 32, 24),
-                  child: Form(
-                    key: _formKey,
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        Text(
-                          _isLogin.value ? 'Inloggen' : 'Registreren',
-                          style: theme.textTheme.headlineMedium,
-                        ),
-                        const SizedBox(height: 16),
-                        TextFormField(
-                          controller: _emailController,
-                          keyboardType: TextInputType.emailAddress,
-                          validator: (String? value) {
-                            if (value == null ||
-                                value.isEmpty ||
-                                !value.contains('@') ||
-                                !value.contains('.')) {
-                              return 'Voer een geldig e-mailadres in';
-                            }
-                            return null;
-                          },
-                          decoration: const InputDecoration(
-                            prefixIcon: Icon(Icons.email_outlined),
-                            labelText: 'E-mailadres',
+                  // FORM.
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(32, 24, 32, 24),
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Text(
+                            _isLogin.value ? 'Inloggen' : 'Registreren',
+                            style: theme.textTheme.headlineMedium,
                           ),
-                        ),
-                        const SizedBox(height: 16),
-                        TextFormField(
-                          controller: _passwordController,
-                          obscureText: true,
-                          validator: (String? value) {
-                            if (value == null || value.length < 6) {
-                              return 'Wachtwoord moet minimaal 6 tekens zijn';
-                            }
-                            return null;
-                          },
-                          decoration: const InputDecoration(
-                            prefixIcon: Icon(Icons.lock_outlined),
-                            labelText: 'Wachtwoord',
+                          const SizedBox(height: 16),
+                          TextFormField(
+                            controller: _emailController,
+                            keyboardType: TextInputType.emailAddress,
+                            validator: (String? value) {
+                              if (value == null ||
+                                  value.isEmpty ||
+                                  !value.contains('@') ||
+                                  !value.contains('.')) {
+                                return 'Voer een geldig e-mailadres in';
+                              }
+                              return null;
+                            },
+                            decoration: const InputDecoration(
+                              prefixIcon: Icon(Icons.email_outlined),
+                              labelText: 'E-mailadres',
+                            ),
                           ),
-                        ),
-                        const SizedBox(height: 24),
-                        FilledButton(
-                          style: ElevatedButton.styleFrom(
-                            minimumSize: const Size(double.infinity, 48),
+                          const SizedBox(height: 16),
+                          TextFormField(
+                            controller: _passwordController,
+                            obscureText: true,
+                            validator: (String? value) {
+                              if (value == null || value.length < 6) {
+                                return 'Wachtwoord moet minimaal 6 tekens zijn';
+                              }
+                              return null;
+                            },
+                            decoration: const InputDecoration(
+                              prefixIcon: Icon(Icons.lock_outlined),
+                              labelText: 'Wachtwoord',
+                            ),
                           ),
-                          onPressed: _submit,
-                          child: _isLoading.value
-                              ? const LinearProgressIndicator()
-                              : Text(
-                                  _isLogin.value ? 'Inloggen' : 'Registreren',
-                                ),
-                        ),
-                        const SizedBox(height: 8),
-                        TextButton(
-                          onPressed: () {
-                            _isLogin.value = !_isLogin.value;
-                          },
-                          child: Text(
-                            _isLogin.value
-                                ? 'Nog geen account?'
-                                : 'Heb je al een account?',
+                          const SizedBox(height: 24),
+                          FilledButton(
+                            style: ElevatedButton.styleFrom(
+                              minimumSize: const Size(double.infinity, 48),
+                            ),
+                            onPressed: _submit,
+                            child: _isLoading.value
+                                ? const LinearProgressIndicator()
+                                : Text(
+                                    _isLogin.value ? 'Inloggen' : 'Registreren',
+                                  ),
                           ),
-                        ),
-                        TextButton(
-                          onPressed: () async {
-                            if (_emailController.text.isNotEmpty) {
-                              await _firebaseService.resetPassword(
-                                _emailController.text.trim().toLowerCase(),
-                              );
-                            } else {
-                              ToastService.showWarning(
-                                title: 'Controleer Emailadres',
-                                subtitle:
-                                    'Vul je emailadres in om een '
-                                    'resetlink te ontvangen.',
-                              );
-                            }
-                          },
-                          child: const Text(
-                            'Wachtwoord vergeten?',
+                          const SizedBox(height: 8),
+                          TextButton(
+                            onPressed: () {
+                              _isLogin.value = !_isLogin.value;
+                            },
+                            child: Text(
+                              _isLogin.value
+                                  ? 'Nog geen account?'
+                                  : 'Heb je al een account?',
+                            ),
                           ),
-                        ),
-                      ],
+                          TextButton(
+                            onPressed: () async {
+                              if (_emailController.text.isNotEmpty) {
+                                await _firebaseService.resetPassword(
+                                  _emailController.text.trim().toLowerCase(),
+                                );
+                              } else {
+                                ToastService.showWarning(
+                                  title: 'Controleer Emailadres',
+                                  subtitle:
+                                      'Vul je emailadres in om een '
+                                      'resetlink te ontvangen.',
+                                );
+                              }
+                            },
+                            child: const Text(
+                              'Wachtwoord vergeten?',
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
